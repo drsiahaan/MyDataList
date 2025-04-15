1b420c4b74f44c86c5b30b872a89e473819c8407
Dicka Reynaldi <dickareynaldisiahaan@gmail.com>
2025-04-15 08:25:35 +0700

    Initial Commit

---
+++ MyDataListTests/MyDataListTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyDataListTests.swift
+//  MyDataListTests
+//
+//  Created by Dicka Reynaldi on 15/04/25.
+//
+
+import Testing
+@testable import MyDataList
+
+struct MyDataListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
